23af3c1493341682e6938c425af3cf407aefe4f6
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-26 00:01:42 +0700

    Update docstring

--- MLs/models/Model.py
+++ MLs/models/Model.py
@@ -6,7 +6,7 @@ class Skel(ABC):
     @abstractmethod
     def face_verification(image) -> bool:
         """
-        @input array of 2 image
+        @param array of 2 image
         @return true indicate that they are the same person
                 false indicate that ther are NOT the same person
         """
