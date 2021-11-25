96fb0a150c8e7e48bf0ed75ba0c3c48fc14c1278
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-25 17:54:30 +0700

    Add feature_utils

---
+++ feature_utils/face_utils.py
@@ -0,0 +1,45 @@
+from mtcnn import MTCNN
+import cv2 as cv
+
+
+def detect_face(image):
+    """
+    @input openCV image object
+    @return array of 'dictionary' of found face(s) inside image with key 'box' : (x, y, width, height)
+    """
+    face_detector = load_mtcnn.mtcnn
+
+    downsize = 2
+
+    image_width = image.shape[1]
+    image_height = image.shape[0]
+
+    resize_image = cv.resize(image, (int(
+        image_width/downsize), int(image_height/downsize)), interpolation=cv.INTER_AREA)
+
+
+    result = face_detector.detect_faces(resize_image)
+
+    for face in result:
+        del face['confidence']
+        del face['keypoints']
+
+        face['box'] = [x * 2 for x in face['box']]
+
+    return result
+
+def crop_face(image, bounding_box):
+    """
+    @input 'image' and 'array' of bounding box of faces inside image
+    @return array of cropped image
+    """
+    cropped_faces = []
+
+    for face in bounding_box:
+        x, y, width, height = face['box']
+        cropped_faces.append(image[y:y + height, x:x + width])
+
+    return cropped_faces
+
+class load_mtcnn():
+    mtcnn = MTCNN()
