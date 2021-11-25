936a8b3aa1302297456807b636aee1bb4be4bd10
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-25 18:33:50 +0700

    Change Model API

--- MLs/models/Model.py
+++ MLs/models/Model.py
@@ -4,7 +4,16 @@ from abc import ABC, abstractmethod
 class Skel(ABC):
 
     @abstractmethod
-    def predict(image):
+    def face_verification(image) -> bool:
+        """
+        @input array of 2 image
+        @return true indicate that they are the same person
+                false indicate that ther are NOT the same person
+        """
+        pass
+
+    @abstractmethod
+    def face_registration():
         pass
 
     def test(self):
