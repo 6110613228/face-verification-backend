20dfe311140b4d462b9a7a7c1ff2fda2dce4907e
Poonnatuch B <6110613228@student.tu.ac.th>
2021-11-25 17:54:54 +0700

    Implement Models and feature_utils

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import cv2 as cv
 import numpy as np
 
+from feature_utils import face_utils
+from Models.Model_Controller import models
+
 app = FastAPI()
 
 origins = [
@@ -21,10 +24,9 @@ app.add_middleware(
 
 @app.get("/")
 async def main():
-    return { "message": "Hello World" }
+    return {"message": "Hello World"}
 
 
-count = 0
 @app.websocket("/ws/{option}")
 async def websocket_endpoint(websocket: WebSocket):
     count = 0
@@ -35,18 +37,22 @@ async def websocket_endpoint(websocket: WebSocket):
             option = websocket.path_params['option']
 
             nparr = np.frombuffer(data, np.uint8)
-            image = cv.imdecode(nparr, -1)
-            #cv.imwrite('test.png', image)
+            image = cv.imdecode(nparr, 1)
+
+            faces_bb = face_utils.detect_face(image)
+            results = face_utils.crop_face(image, faces_bb)
+
+            for i, f in enumerate(results):
+                cv.imwrite('image' + str(i) + '.png', f)
+            model = models['wave']
 
             #img_str = cv.imencode('.png', image)[1].tobytes()
 
-            print('Got image', option)
-            count += 1
             await websocket.send_json({
                 'message': 'test',
                 'face_is_found': True,
-                'count_face': count,
-                'found_faces_bb' : []
+                'count_face': 0,
+                'found_faces_bb': faces_bb
             })
 
         except WebSocketDisconnect:
